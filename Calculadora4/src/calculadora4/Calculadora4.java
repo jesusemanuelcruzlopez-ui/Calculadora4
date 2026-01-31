5169df7fc477ed9c794365c8d525a0b15be6c0cc
Pc4 <Pc4@CPU4>
2026-01-31 10:24:12 -0600

    codigoPrincipal4

---
+++ Calculadora4/src/calculadora4/Calculadora4.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadora4;
+
+import java.util.Scanner;
+
+public class Calculadora4 {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner (System.in);
+        
+       System.out.println("*************************************");
+       System.out.println("* ¿Tadeo donde estas que no te veo? *");
+       System.out.println("*************************************");
+       
+         System.out.print("Ingresa el primer numero: ");
+         int a = sc.nextInt();
+         System.out.println("Ingresa tu segundo numero: ");
+         int b = sc.nextInt();
+       
+         /*
+          Suma suma= new Suma();       
+          Resta resta= new Resta();       
+          Multiplicacion multiplicacion= new Multiplicacion();       
+          Divicion divicion= new Divicion();       
+
+       System.out.println("La suma es: "+ suma.);
+       System.out.println("La resta es: "+ resta.);
+       System.out.println("La multiplicacion es: "+ multiplicacion.);
+       System.out.println("La divicion es: "+ divicion.);
+       */
+         
+       
+    }
+    
+}
